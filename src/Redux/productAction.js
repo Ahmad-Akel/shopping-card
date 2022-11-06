2a2a7393297f6cafb84802b277be75b9c18ca112
Ahmad Akel <ahmad.oudai1999@gmail.com>
2022-11-06 11:31:40 +0100

    List Products in UI

--- src/Redux/productAction.js
+++ src/Redux/productAction.js
@@ -1,7 +1,7 @@
 import { PRODUCT_LIST } from "./constants";
 
 export const productData = () => {
-  let data = "hello";
+  let data = "Hello";
   console.log("CALL_PRODUCT_LIST is called", data);
   return {
     type: PRODUCT_LIST,
